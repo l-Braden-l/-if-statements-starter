8a1ea368ed2e447b499a822fb8e166ec0baf492f
braden <leachbr787@northwested.org>
2024-09-30 14:12:25 -0400

    Update main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+#Braden Leach
+#Sep 30 2024
+#Station Rotation
+
+#task 1
+superheros = ['super man','spider man','iron man','wolverine']
+fav_superheros = 'spider man'
+if fav_superheros in superheros:
+      print('your favorite super hero is spider man!')
+
+
+#task 2 
+quiz = 90
+if quiz > 85:
+        print(f'Your score was {quiz} so your earned a A!')
+#task 3
+quote = "Roads? Where we're going we don't need roads"
+if quote.startswith('M'):
+        print('Your quote starts with an uppercase M')
+
+#task 4
+filename = 'family.png'
+if filename.endswith('.png'):
+    print(f'Your file {filename}, ends with the .png extension')
+#task 5
+is_registred = True
+if is_registred:
+    print('You are registered for classes')
